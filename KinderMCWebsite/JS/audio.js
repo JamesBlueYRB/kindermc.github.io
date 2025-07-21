3d3e692862eb1dc11ac768c92ad291c7b25062d9
JamesBlueYRB <140871276+JamesBlueYRB@users.noreply.github.com>
2025-07-21 22:38:40 +0700

    Delete KinderMCWebsite directory

--- KinderMCWebsite/JS/audio.js
+++
@@ -1,6 +0,0 @@
-const audio = document.createElement('audio');
-audio.src = 'Met Her on the internet.mp3';
-audio.loop = true;
-audio.autoplay = true;
-audio.id = 'myAudio';
-document.body.appendChild(audio);
